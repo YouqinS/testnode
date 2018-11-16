79ba1d9da9cec7333f49564bd1b617afd37661a8
YouqinS <33859677+YouqinS@users.noreply.github.com>
2018-11-16 11:22:35 +0200

    upload dir

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ app.use(express.static('public'));
 app.use('/', express.static(__dirname + '/public')); //https://expressjs.com/en/starter/static-files.html
 
 
-
 const multer = require('multer');//https://github.com/expressjs/multer
 const upload = multer({test:'upload/'});
 
