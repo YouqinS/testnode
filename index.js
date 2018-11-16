f7421a6af94ef830f67c361c83bd5eb81c65e146
YouqinS <33859677+YouqinS@users.noreply.github.com>
2018-11-16 13:10:19 +0200

    store file

--- index.js
+++ index.js
@@ -17,12 +17,52 @@ app.use('/', express.static(__dirname + '/public')); //https://expressjs.com/en/
 const multer = require('multer');//https://github.com/expressjs/multer
 const upload = multer({test:'upload/'});
 
+//MULTER CONFIG: to get file photos to temp server storage
+const multerConfig = {
+
+  //specify diskStorage (another option is memory)
+  storage: multer.diskStorage({
+
+    //specify destination
+    destination: function(req, file, next){
+      next(null, './public/photos');
+    },
+
+    //specify the filename to be unique
+    filename: function(req, file, next){
+      console.log(file);
+      //get the file mimetype ie 'image/jpeg' split and prefer the second value ie'jpeg'
+      const ext = file.mimetype.split('/')[1];
+      //set the file fieldname to a unique name containing the original name, current datetime and the extension.
+      next(null, file.fieldname + '-' + Date.now() + '.'+ext);
+    }
+  }),
+
+  // filter out and prevent non-image files.
+  fileFilter: function(req, file, next){
+    if(!file){
+      next();
+    }
+
+    // only permit image mimetypes
+    const image = file.mimetype.startsWith('image/');
+    if(image){
+      console.log('photo uploaded');
+      next(null, true);
+    }else{
+      console.log("file not supported")
+      //TODO:  A better message response to user on failure.
+      return next();
+    }
+  }
+};
 
 
- app.get('/', (req, res) => { //call function get  //request from client, response from server
+ app.get('/', (req, res) => {
    res.send('Hello testing GET REQUEST');
    console.log('server gets called')
  });
+
 
 app.get('/', function(req, res){
   res.render('task_b.html');
@@ -40,8 +80,10 @@ app.post('/', (req, res)=>{
 
 
 
-app.post('/profile', upload.single('avatar'), function (req, res) {
-  // req.file is the `avatar` file
+//app.post('/upload', upload.single('avatar'), function (req, res) {
+  app.post('/upload', multer(multerConfig).single('avatar'),function(req, res){
+
+    // req.file is the `avatar` file
   // req.body will hold the text fields, if there were any
   res.send("uploadding file");
 
